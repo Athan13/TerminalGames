493edf4daa193259336bc8d032f173f1024b6b57
Athan Massouras <amassouras@berkeley.edu>
2024-01-08 01:49:52 +0100

    bug fix: newline done, sped up, kind of messy but it's ok it's done now

--- Speedtyping/speedtyping.c
+++ Speedtyping/speedtyping.c
@@ -8,7 +8,7 @@
 
 #include <ncurses.h>
 
-#define MAX_WIDTH 100
+#define MAX_WIDTH 70
 
 char* get_ref_string() {
     srand(time(NULL));
@@ -16,7 +16,7 @@ char* get_ref_string() {
 
     char* filepath = malloc(11 * sizeof(char));
     sprintf(filepath, "texts/input%d.txt", file_n);
-    FILE* text_f = fopen(filepath, "r");
+    FILE* text_f = fopen("texts/input0.txt", "r");
     free(filepath);
 
     fseek(text_f, 0, SEEK_END);
@@ -59,6 +59,9 @@ char* format_ref_string(int start, size_t length, char* ref_string) {
     char* return_c;
     size_t ref_length = strlen(ref_string);
 
+    int next_newline = strchr(ref_string + (start * sizeof(char)), '\n') - (ref_string + (start * sizeof(char))) + 1;
+    length = next_newline < length ? next_newline : length;
+
     if (start >= ref_length || start < 0) {
         return_c = calloc(1, sizeof(char));
         strncpy(return_c, "", 0);
@@ -71,12 +74,6 @@ char* format_ref_string(int start, size_t length, char* ref_string) {
     }
 
     return return_c;
-}
-
-void clear_str(char* input_str, char replacement) {
-    int len = strlen(input_str);
-    memset(input_str, replacement, len);
-    input_str[len] = '\0';
 }
 
 int main(int argc, char *argv[]) {
@@ -106,11 +103,11 @@ int main(int argc, char *argv[]) {
 
     int width = COLS > MAX_WIDTH ? MAX_WIDTH : COLS;
 
-    WINDOW* instructions_test = newwin(1, width, LINES - 11, 0);
+    WINDOW* instructions_text = newwin(1, width, LINES - 11, 0);
     WINDOW* ref_text_window = newwin(6, width, LINES - 10, 0);
     WINDOW* input_window = newwin(3, width, LINES - 4, 0);
 
-    nodelay(instructions_test, true);
+    nodelay(instructions_text, true);
     nodelay(ref_text_window, true);
     nodelay(input_window, true);
 
@@ -119,8 +116,9 @@ int main(int argc, char *argv[]) {
     char ref_c;
     int correct_chars = 0;
 
-    int ref_text_pos = 0;
+    int line_pos = 0;
     int input_word_pos = 0;
+    int ref_text_pos = 0;
 
     char input_word[width - 15];
     memset(input_word, '\0', sizeof(input_word));
@@ -128,51 +126,60 @@ int main(int argc, char *argv[]) {
     int wpm = 0;
 
     // set up string formatter
-    int line_counter = 0;
     char* line0 = format_ref_string(-1, width - 2, ref_string);
     char* line1 = format_ref_string(0, width - 2, ref_string);
-    char* line2 = format_ref_string(width - 2, width - 2, ref_string);
+    char* line2 = format_ref_string(strlen(line1), width - 2, ref_string);
+
+    // initial set up display
+    mvwprintw(instructions_text, 0, 0, "Type as fast as you can! Press ESCAPE to exit.\n");
+    wrefresh(instructions_text);
+
+    mvwprintw(ref_text_window, 2, 1, line0);
+    mvwprintw(ref_text_window, 3, 1, line1);
+    mvwprintw(ref_text_window, 4, 1, line2);
+    draw_borders(ref_text_window);
+    wrefresh(ref_text_window);
+
+    draw_borders(input_window);
+    mvwprintw(input_window, 1, 1, "Type here:");
+    wrefresh(input_window);
 
     // main game loop
     while(time >= 0) {
-        mvwprintw(instructions_test, 0, 0, "Type as fast as you can! Press ESCAPE to exit.\n");
-        wrefresh(instructions_test);
-
-        // draw reference text window
-        draw_borders(ref_text_window);
-
-        mvwprintw(ref_text_window, 1, 1, "Time remaining: %d", time);
-        wpm = 12 * correct_chars / (start_time - time);
-        mvwprintw(ref_text_window, 1, width - 9, "WPM: %d", wpm);
-
-        mvwprintw(ref_text_window, 2, 1, line0);
-        mvwprintw(ref_text_window, 3, 1, line1);
-        mvwprintw(ref_text_window, 4, 1, line2);
-
-        wrefresh(ref_text_window);
-
-        // draw input window
-        draw_borders(input_window);
-        mvwprintw(input_window, 1, 1, "Type here: %s", input_word);
-        wrefresh(input_window);
-
         // compute time
         if ((double) clock() - start >= 1000000) {
             start = clock();
             time--;
+
+            mvwprintw(ref_text_window, 1, 1, "Time remaining: %d", time);
+            wpm = 12 * correct_chars / (start_time - time);
+            mvwprintw(ref_text_window, 1, width - 9, "WPM: %d", wpm);
+
+            wrefresh(ref_text_window);
         }
 
         // add string
-        ref_c = line1[ref_text_pos];
+        ref_c = line1[line_pos];
         if (ref_c == '\0') {
-            line_counter++;
-
             free(line0);
             line0 = line1;
             line1 = line2;
-            line2 = format_ref_string((line_counter + 1) * (width - 2), width - 2, ref_string);
-            ref_text_pos = 0;
-            clear_str(input_word, ' ');
+            line2 = format_ref_string(ref_text_pos + strlen(line1), width - 2, ref_string);
+            line_pos = 0;
+
+            werase(ref_text_window);
+
+            mvwprintw(ref_text_window, 2, 1, line0);
+            mvwprintw(ref_text_window, 3, 1, line1);
+            mvwprintw(ref_text_window, 4, 1, line2);
+
+            mvwprintw(ref_text_window, 1, 1, "Time remaining: %d", time);
+            wpm = 12 * correct_chars / (start_time - time);
+            mvwprintw(ref_text_window, 1, width - 9, "WPM: %d", wpm);
+
+            draw_borders(ref_text_window);
+
+            wrefresh(ref_text_window);
         }
 
         char in_c = wgetch(input_window);
@@ -181,18 +188,33 @@ int main(int argc, char *argv[]) {
         } else if (in_c == ref_c) {
             input_word[input_word_pos] = in_c;
             input_word[input_word_pos + 1] = '\0';
+
+            mvwprintw(input_window, 1, 12, input_word);
+            wrefresh(input_window);
+
             input_word_pos++;
+            line_pos++;
             ref_text_pos++;
 
             if (!isspace(in_c)) {
                 correct_chars++;
             } else {
-                clear_str(input_word, ' ');
+                werase(input_window);
+                draw_borders(input_window);
+                mvwprintw(input_window, 1, 1, "Type here:");
+                wrefresh(input_window);
+
+                input_word[0] = '\0';
                 input_word_pos = 0;
             }
 
             if (input_word_pos == max_length - 1) {
-                clear_str(input_word, ' ');
+                werase(input_window);
+                draw_borders(input_window);
+                mvwprintw(input_window, 1, 1, "Type here:");
+                wrefresh(input_window);
+
+                input_word[0] = '\0';
                 input_word_pos = 0;
             }
         }
@@ -202,6 +224,7 @@ int main(int argc, char *argv[]) {
 
     // Cleanup
     free(ref_string);
+    delwin(instructions_text);
     delwin(ref_text_window);
     delwin(input_window);
 
